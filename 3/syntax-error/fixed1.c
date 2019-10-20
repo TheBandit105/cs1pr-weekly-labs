8ad282ed4279cc2314596afd30429283dcfd5342
Shavin Croos <at015244@student.reading.ac.uk>
2019-10-20 19:53:32 +0100

    Upload New File

---
+++ 3/syntax-error/fixed1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]){
+  for(int i=0; i < argc; i++){
+
+printf("%d", argv[i]);
+
+}
+
+  printf("\n");
+
+  /*Now we end the program*/
+
+  return 0;
+}
+
