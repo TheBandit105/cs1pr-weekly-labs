94a9494f180d19b194455328ae615b6b512a814b
Shavin Croos <at015244@student.reading.ac.uk>
2019-10-27 23:05:59 +0000

    Upload New File

---
+++ 4/strtoint/adder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "my_coversion.h"
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+
+printf("Please type in two numbers to be added: ")
+scanf("%d", num1, num2)
+
+printf("%d\n", conversion (int num1 + int num2));
+
+  return 0;
+}
