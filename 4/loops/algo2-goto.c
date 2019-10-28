0041f3ea4505c8e1b395436c89ce044c3e6ce305
Shavin Croos <at015244@student.reading.ac.uk>
2019-10-28 11:28:39 +0000

    Replace algo2-goto.c

--- 4/loops/algo2-goto.c
+++ 4/loops/algo2-goto.c
@@ -10,10 +10,9 @@ int main (int argc, char ** argv) // This part of the program declares the main 
   int n = strlen(str); // This declares n as a integer which is assigned to the string length of the variable string.
   int i = 0; // This declares i as an integer which is assigned the value of 0.
   loop: ;
-  (str[i] != str[n-i-1]) // Determines if string array of i is not equal to the string array of strlen(str) - i - 1.
+  (str[i] != str[n-i-1]); // Determines if string array of i is not equal to the string array of strlen(str) - i - 1.
   {
-    pldrm = false;
-    break; // Another way of terminating a function of program besides return 0;.
+    pldrm = false;./
     i++;
   }
   if(i < n/2) // If this if condition is met, then the if function will go to the loop function, which will declare the pldrm as false and increase the variable i by 1.
@@ -22,11 +21,11 @@ int main (int argc, char ** argv) // This part of the program declares the main 
   }
   if(pldrm) // If the boolean variable pldrm still returns as true even after going through the loop function, then the compiler will print 'printf("This is a palindrome.\n")'.
   {
-    printf("This is a palindrome.\n")
+    printf("This is a palindrome.\n");
   }
   else // If the boolean variable pldrm still returns as false, then the compiler prints 'printf("Error! This is not a palindrome.\n")'.
   {
-    printf("Error! This is not a palindrome.\n")
+    printf("Error! This is not a palindrome.\n");
     return 0; // Terminates program.
   }
 }
